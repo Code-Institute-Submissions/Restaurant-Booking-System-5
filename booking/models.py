d82fe9e1b6abdd257407ec699103a28776610fab
DerekMor <derekmoriarty24@gmail.com>
2023-05-14 23:29:37 +0000

    Add timeSlot model class to admin

--- booking/models.py
+++ booking/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Slot(models.Model):
-    day = models.DecimalField(null=False)
-    time = models.DecimalField(null=False)
+    day = models.CharField(null=False, max_length=10)
+    time = models.DecimalField(null=False, decimal_places=2, max_digits=5)
     booked = models.BooleanField(default=False)
