298a4ff8762fc272708e3d0ede16f39fc79d3505
DerekMor <>
2023-05-14 21:44:26 +0000

    Get ready for first deployment

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def displayPage(request):
+    return render(request, 'booking/booking.html')
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo.views import displayPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', displayPage, name = 'displayPage')
 ]
