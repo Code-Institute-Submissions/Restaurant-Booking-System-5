6b5c1384f935f3e33ee4e7157af166fab96535e9
DerekMor <derekmoriarty24@gmail.com>
2023-05-15 20:56:00 +0000

    Add large html table for bookings

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Slot(models.Model):
     day = models.CharField(null=False, max_length=10)
     time = models.DecimalField(null=False, decimal_places=2, max_digits=5)
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+
 def displayPage(request):
     return render(request, 'booking/booking.html')
