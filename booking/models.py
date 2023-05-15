035f7921ab5453948b6184f704870d74bab1e740
DerekMor <derekmoriarty24@gmail.com>
2023-05-15 21:36:40 +0000

    Add base.html file, nav bar, and copy head section to base

--- booking/models.py
+++ booking/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Slot(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     day = models.CharField(null=False, max_length=10)
     time = models.DecimalField(null=False, decimal_places=2, max_digits=5)
     booked = models.BooleanField(default=False)
