d7623e3bd329f441f8ae94c4f7cdf4466d85eaf4
DerekMor <derekmoriarty24@gmail.com>
2023-05-16 23:18:53 +0000

    setting default booked to True to test deployment server

--- booking/models.py
+++ booking/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Slot(models.Model):
     day = models.CharField(null=False, max_length=10)
     time = models.DecimalField(null=False, decimal_places=2, max_digits=5)
-    booked = models.BooleanField(default=False)
+    booked = models.BooleanField(default=True)
 
 
 class Booking(models.Model):
