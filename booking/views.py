32a14af5bf2dd58cb60569c1b843e397fc373277
DerekMor <derekmoriarty24@gmail.com>
2023-05-16 00:21:39 +0000

    test function for booking in heroku

--- booking/views.py
+++ booking/views.py
@@ -9,4 +9,26 @@ from django.contrib import messages
 
 def displayPage(request):
     return render(request, 'booking/booking.html')
+@login_required
+def book_slot(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            day = form.cleaned_data['day']
+            time = form.cleaned_data['time']
+            
+            # Check if time slot is already booked
+            if Booking.objects.filter(day=day, time=time).exists():
+                error_message = "The selected time slot is already booked."
+                return render(request, 'booking/book_slot.html', {'form': form, 'error_message': error_message})
+            
+            # Create a new booking 
+            booking = Booking(user=request.user, day=day, time=time)
+            booking.save()
+            
+            # Redirect to a success page or display a success message
+            return redirect('booking:booking_success')
+    else:
+        form = BookingForm()
 
+    return render(request, 'booking/book_slot.html', {'form': form})
